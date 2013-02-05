b54c1ef3f3714d13d4be5d909914dd34ddb156f9
Daniel Z <xzdasx@gmail.com>
2013-02-04 21:12:17 -0430

    actiones para representantes

---
+++ app/controllers/representatives_controller.rb
@@ -0,0 +1,31 @@
+class RepresentativesController < ApplicationController
+  before_filter :get_representative, :only => [:show,:edit,:update,:destroy]
+
+  def index
+    @representatives = Representative.all
+  end
+
+  def show
+  end
+
+  def new
+    @representative = Representative.new
+  end
+
+  def create
+    @representative = Representative.create(params[:representative])
+    redirect_to representative_path(@representative)
+  end
+
+  def edit
+  end
+
+  def update
+    @representative.update_attributes(params[:representative])
+    redirect_to representative_path(@representative)
+  end
+
+  def get_representative
+    @representative = Representative.find(params[:id])
+  end
+end
--- app/models/representative.rb
+++ app/models/representative.rb
@@ -1,6 +1,11 @@
 class Representative < ActiveRecord::Base
-  attr_accessible :dni, :mobile_number, :observation, :phone_number
+  attr_accessible :dni, :mobile_number, :observation, :phone_number,:last_name,:first_name
 
   has_many :representative_students
   has_many :students, :through => :representative_students
+
+
+  def to_s
+    "#{first_name} #{last_name}"
+  end
 end
